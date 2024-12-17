b87394d48d908d3f6a5ed2ed2d686256c27d3fe4
NaveedKhan09 <naveedkhan10990@gmail.com>
2024-12-17 16:56:00 +0500

    add new section for completed tasks

--- script.js
+++ script.js
@@ -2,29 +2,49 @@ let taskButton = document.querySelector("#addTask");
 let input = document.querySelector("#task");
 let inputValue = input.value;
 let taskContainer = document.querySelector(".Alltasks");
+let completedTasksdiv = document.querySelector(".completedTask")
 
+//-----------adding new task-------------------------
 
 taskButton.onclick = function Add(){
     if(input.value == 0){
         alert("Please enter a Task");
     }else{
-        //-----Elements Creation-----
+    //-----------Elements Creation--------------------
         let taskDiv = document.createElement("div");
         let paragraph = document.createElement("p");
+        let buttonDiv = document.createElement("div");
         let delButton = document.createElement("button");
         delButton.innerText = 'Delete';
-        //-------Append Child------------
+        let completeTaskbtn = document.createElement("button");
+        completeTaskbtn.innerText = 'Complete';
+
+    //-------------------Append Child-----------------
+
         taskContainer.appendChild(taskDiv);
         taskDiv.appendChild(paragraph);
-        taskDiv.appendChild(delButton);
+        taskDiv.appendChild(buttonDiv);
+        buttonDiv.appendChild(completeTaskbtn);
+        buttonDiv.appendChild(delButton);
         paragraph.innerText = input.value;
         taskDiv.classList.add("newTask");
+        buttonDiv.classList.add("btnsdiv");
         delButton.classList.add("delbtn");
-
+        completeTaskbtn.classList.add("completebtn");
         input.value = "";
-        //------------------------delete task---------
+    //------------------------delete task-------------
+    
         delButton.onclick =  deleteTask=()=>{
             taskDiv.remove();
         } 
+    //------------------------complete task-----------
+
+        completeTaskbtn.onclick = taskcompleted=()=>{
+            completeTaskbtn.innerText = 'Completed';
+            completeTaskbtn.style.backgroundColor = "green";
+            completeTaskbtn.disabled = true;
+            taskDiv.remove();
+            completedTasksdiv.appendChild(taskDiv);
+        }
     } 
 }
